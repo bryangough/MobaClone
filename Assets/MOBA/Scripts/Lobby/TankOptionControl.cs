debbe0c1e12adc6fa7450f758edc447693239f10
Bryan Gough <gough.bryan@gmail.com>
2017-12-04 19:33:02 -0500

    Creating prototype of lobby

--- Assets/MOBA/Scripts/Lobby/TankOptionControl.cs
+++ Assets/MOBA/Scripts/Lobby/TankOptionControl.cs
@@ -7,5 +7,22 @@ public class TankOptionControl : MonoBehaviour {
 	public BaseOption option;
 	public Text label;
 	public Image displayImage;
+	public Toggle toggle;
+
+	void Start()
+	{
+		toggle = this.GetComponent<Toggle>();
+		ToggleGroup toggleGroup = this.GetComponentInParent<ToggleGroup>();
+		if( toggleGroup != null )
+		{
+			toggle.group = toggleGroup;
+			toggle.isOn = false;
+		}
+		if( option!= null)
+		{
+			label.text = option.optionName;
+			displayImage.sprite = option.ui_image;
+		}
+	}
 	
 }
